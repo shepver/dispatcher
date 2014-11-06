a426a64acfa22a2ee04e4d6dc9ca1927680fb1c8
Mikhail Gugnin <mikhail.gugnin@suls.ru>
2014-11-06 09:57:11 +0800

    .

--- src/executor.erl
+++ src/executor.erl
@@ -16,11 +16,11 @@
 
 %% gen_server callbacks
 -export([init/1,
-   handle_call/3,
-   handle_cast/2,
-   handle_info/2,
-   terminate/2,
-   code_change/3]).
+  handle_call/3,
+  handle_cast/2,
+  handle_info/2,
+  terminate/2,
+  code_change/3]).
 
 -define(SERVER, ?MODULE).
 
@@ -38,9 +38,9 @@
 %% @end
 %%--------------------------------------------------------------------
 -spec(start_link(Args :: term()) ->
-   {ok, Pid :: pid()} | ignore | {error, Reason :: term()}).
+  {ok, Pid :: pid()} | ignore | {error, Reason :: term()}).
 start_link(Script) ->
-   gen_server:start_link({local, Script#script.name}, ?MODULE, [Script], []).
+  gen_server:start_link({local, Script#script.name}, ?MODULE, [Script], []).
 
 %%%===================================================================
 %%% gen_server callbacks
@@ -58,15 +58,15 @@ start_link(Script) ->
 %% @end
 %%--------------------------------------------------------------------
 -spec(init(Args :: term()) ->
-   {ok, State :: #state{}} | {ok, State :: #state{}, timeout() | hibernate} |
-   {stop, Reason :: term()} | ignore).
+  {ok, State :: #state{}} | {ok, State :: #state{}, timeout() | hibernate} |
+  {stop, Reason :: term()} | ignore).
 init([Script]) ->
-   case file:open(lists:concat(["log/", Script#script.name, ".log"]), [append]) of
-      {ok, IoDevice} ->
-         Timer = erlang:send_after(10, self(), {run, Script#script.name}),
-         {ok, #state{timer = Timer, log = IoDevice, script = Script}};
-      {error, Reason} -> {stop, Reason}
-   end.
+  case file:open(lists:concat(["log/", Script#script.name, ".log"]), [append]) of
+    {ok, IoDevice} ->
+      Timer = erlang:send_after(10, self(), {run, Script#script.name}),
+      {ok, #state{timer = Timer, log = IoDevice, script = Script}};
+    {error, Reason} -> {stop, Reason}
+  end.
 
 %%--------------------------------------------------------------------
 %% @private
@@ -79,39 +79,40 @@ init([Script]) ->
 %% @end
 %%--------------------------------------------------------------------
 -spec(handle_info(Info :: timeout() | term(), State :: #state{}) ->
-   {noreply, NewState :: #state{}} |
-   {noreply, NewState :: #state{}, timeout() | hibernate} |
-   {stop, Reason :: term(), NewState :: #state{}}).
+  {noreply, NewState :: #state{}} |
+  {noreply, NewState :: #state{}, timeout() | hibernate} |
+  {stop, Reason :: term(), NewState :: #state{}}).
 
 
 handle_info({run, Name}, #state{timer = Timer, script = Script} = State) ->
-   erlang:cancel_timer(Timer),
-   Port = open_port({spawn, Script#script.command},
-      [{line, 100}, exit_status, stderr_to_stdout, binary]),
-   error_logger:info_msg("script start work ~p .~n", [Port]),
-   NewTimer = erlang:send_after(Script#script.interval * 1000, self(), {run, Name}),
-   {noreply, State#state{port = Port, timer = NewTimer}}
+  erlang:cancel_timer(Timer),
+  Port = open_port({spawn, Script#script.command},
+    [{line, 100}, exit_status, stderr_to_stdout, binary]),
+  error_logger:info_msg("script start work ~p .~n", [Port]),
+  NewTimer = erlang:send_after(Script#script.interval * 1000, self(), {run, Name}),
+  {noreply, State#state{port = Port, timer = NewTimer}}
 ;
 
 
 handle_info({_Port, {data, Data}}, #state{log = IoFile} = State) ->
-   case Data of
-      {eol, Line} ->
-         file:write(IoFile, Line),
-         file:write(IoFile, <<"\n">>),
-         error_logger:info_msg("Message ~ts .~n", [Line]);
-      {noeol, Line} ->
-         file:write(IoFile, Line),
-         error_logger:info_msg("Message ~p .~n", [Line])
-   end,
-   {noreply, State};
+  case Data of
+    {eol, Line} ->
+      file:write(IoFile, Line),
+      file:write(IoFile, <<"\n">>),
+      error_logger:info_msg("Message ~ts .~n", [Line]);
+    {noeol, Line} ->
+      file:write(IoFile, Line),
+      file:write(IoFile, <<"\n">>),
+      error_logger:info_msg("Message ~p .~n", [Line])
+  end,
+  {noreply, State};
 
 handle_info({Port, {exit_status, Status}}, State) ->
-   error_logger:info_msg("script finish work ~p status ~p.~n", [Port, Status]),
-   {noreply, State};
+  error_logger:info_msg("script finish work ~p status ~p.~n", [Port, Status]),
+  {noreply, State};
 
 handle_info(_Info, State) ->
-   {noreply, State}.
+  {noreply, State}.
 
 %%--------------------------------------------------------------------
 %% @private
@@ -125,9 +126,10 @@ handle_info(_Info, State) ->
 %% @end
 %%--------------------------------------------------------------------
 -spec(terminate(Reason :: (normal | shutdown | {shutdown, term()} | term()),
-      State :: #state{}) -> term()).
-terminate(_Reason, _State) ->
-   ok.
+    State :: #state{}) -> term()).
+terminate(_Reason, #state{log = IoFile} = _State) ->
+  file:close(IoFile),
+  ok.
 
 %%--------------------------------------------------------------------
 %% @private
@@ -138,10 +140,10 @@ terminate(_Reason, _State) ->
 %% @end
 %%--------------------------------------------------------------------
 -spec(code_change(OldVsn :: term() | {down, term()}, State :: #state{},
-      Extra :: term()) ->
-   {ok, NewState :: #state{}} | {error, Reason :: term()}).
+    Extra :: term()) ->
+  {ok, NewState :: #state{}} | {error, Reason :: term()}).
 code_change(_OldVsn, State, _Extra) ->
-   {ok, State}.
+  {ok, State}.
 
 
 %%--------------------------------------------------------------------
@@ -152,15 +154,15 @@ code_change(_OldVsn, State, _Extra) ->
 %% @end
 %%--------------------------------------------------------------------
 -spec(handle_call(Request :: term(), From :: {pid(), Tag :: term()},
-      State :: #state{}) ->
-   {reply, Reply :: term(), NewState :: #state{}} |
-   {reply, Reply :: term(), NewState :: #state{}, timeout() | hibernate} |
-   {noreply, NewState :: #state{}} |
-   {noreply, NewState :: #state{}, timeout() | hibernate} |
-   {stop, Reason :: term(), Reply :: term(), NewState :: #state{}} |
-   {stop, Reason :: term(), NewState :: #state{}}).
+    State :: #state{}) ->
+  {reply, Reply :: term(), NewState :: #state{}} |
+  {reply, Reply :: term(), NewState :: #state{}, timeout() | hibernate} |
+  {noreply, NewState :: #state{}} |
+  {noreply, NewState :: #state{}, timeout() | hibernate} |
+  {stop, Reason :: term(), Reply :: term(), NewState :: #state{}} |
+  {stop, Reason :: term(), NewState :: #state{}}).
 handle_call(_Request, _From, State) ->
-   {reply, ok, State}.
+  {reply, ok, State}.
 
 %%--------------------------------------------------------------------
 %% @private
@@ -170,11 +172,11 @@ handle_call(_Request, _From, State) ->
 %% @end
 %%--------------------------------------------------------------------
 -spec(handle_cast(Request :: term(), State :: #state{}) ->
-   {noreply, NewState :: #state{}} |
-   {noreply, NewState :: #state{}, timeout() | hibernate} |
-   {stop, Reason :: term(), NewState :: #state{}}).
+  {noreply, NewState :: #state{}} |
+  {noreply, NewState :: #state{}, timeout() | hibernate} |
+  {stop, Reason :: term(), NewState :: #state{}}).
 handle_cast(_Request, State) ->
-   {noreply, State}.
+  {noreply, State}.
 
 
 %%%===================================================================
